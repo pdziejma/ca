6170e79cd4cde7f8b7170c9f2fdc9edbae5df131
pdziejma <49507546+pdziejma@users.noreply.github.com>
2019-06-18 14:10:41 -0400

    Add files via upload

--- assign2/app/routes/index.server.routes.js
+++ assign2/app/routes/index.server.routes.js
@@ -1,6 +1,5 @@
 module.exports = function(app) {
     var animals = require('../controllers/animals.server.controller');
     var index = require('../controllers/index.server.controller');
-    app.route('/')
-    .get(index.render);
+    app.get('/', index.render);
 };
